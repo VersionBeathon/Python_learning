fe8739e57065a782057832f2dc295d18e55ea35b
VersionBeathon <s875766683@hotmail.com>
2016-02-27 15:21:38 +0800

    Init

---
+++ chapyer_7/polymorphism.py
@@ -0,0 +1,18 @@
+# _*_ coding:utf-8 _*_
+# 多态和方法
+# 绑定到对象特性上面的函数称为方法(method)。例如：字符串、列表和字典方法
+print 'abc'.count('a')
+print[1, 2, 'a'].count('a')
+# 标准库random中包含choice函数，随机选出元素
+from random import choice
+x = choice(['Hello, world !', [1, 2, 3, 4, 'e', 'e', '4']])
+print x.count('e')
+# 关键点不在于检测类型：只需要知道x有个叫做count的方法。带有一个字符作为参数，并且返回参数值就够了。
+
+# 多态的多种形式
+
+
+def add(x, y):
+    return x + y
+print add(1, 2)
+print add('Fish', 'license')
