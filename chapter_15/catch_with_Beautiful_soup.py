7a83a174d609a590f6365fb8f6e43aa8f0d94acc
songmingyang <songmingyang@51jk.com>
2016-04-26 11:06:21 +0800

    Init

--- chapter_15/catch_with_Beautiful_soup.py
+++ chapter_15/catch_with_Beautiful_soup.py
@@ -1,9 +1,11 @@
 # _*_ coding:utf-8 _*_
 from urllib import urlopen
 from bs4 import BeautifulSoup
+import lxml
 
 text = urlopen('http://python.org/community/jobs').read()
-soup = BeautifulSoup(text)
+
+soup = BeautifulSoup(text, "lxml")
 
 jobs = set()
 for header in soup('h3'):
